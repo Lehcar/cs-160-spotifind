2f929cc712458c962f0c5c6fdd334ecadf950319
unknown <ra.stanik@hotmail.com>
2019-10-09 10:34:31 -0700

    Started using spotipy, authorization not working properly

---
+++ spotifind/client.py
@@ -0,0 +1,50 @@
+import spotipy
+
+sp = spotipy.Spotify()
+
+# import spotipy
+# import spotipy.oauth2 as oauth2
+
+# credentials = oauth2.SpotifyClientCredentials(
+#         client_id=client_id,
+#         client_secret=client_secret)
+#
+# token = credentials.get_access_token()
+# spotify = spotipy.Spotify(auth=token)
+
+import spotipy.util as util
+
+scope = 'user-top-read'
+your_username = 'thepunisherspunishing'
+
+token = util.prompt_for_user_token(
+        username=your_username,
+        scope=scope,
+        client_id='98e1af2c2aad45ecad9997543623cbbf',
+        client_secret='bf63ae87996f4f1aad31335abb361d4e',
+        redirect_uri='https://www.getpostman.com/oauth2/callback')
+
+spotify = spotipy.Spotify(auth=token)
+
+# results = sp.search(q='weezer', limit=20)
+# for i, t in enumerate(results['tracks']['items']):
+#     print(' ', i, t['name'])
+
+top_artists = sp.current_user_top_artists(limit=50, offset=0, time_range='long_term')
+
+
+#
+# current_user_top_artists(limit=20, offset=0, time_range='medium_term')
+# Get the current user’s top artists
+#
+# Parameters:
+# limit - the number of entities to return
+# offset - the index of the first entity to return
+# time_range - Over what time frame are the affinities computed Valid-values: short_term, medium_term, long_term
+# current_user_top_tracks(limit=20, offset=0, time_range='medium_term')
+# Get the current user’s top tracks
+#
+# Parameters:
+# limit - the number of entities to return
+# offset - the index of the first entity to return
+# time_range - Over what time frame are the affinities computed Valid-values: short_term, medium_term, long_term
