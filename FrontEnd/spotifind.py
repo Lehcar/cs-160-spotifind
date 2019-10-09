17a66e56ca7af884b6a28a7565ddb462a7204a51
William Barrera <willybarrera222@gmail.com>
2019-10-09 11:21:11 -0700

    Test commit, also added another function for testing

--- FrontEnd/spotifind.py
+++ FrontEnd/spotifind.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/login')
+def login_page():
+    return 'Click Here to log in!'
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
