039abb4f603598364aca984228111fa458838e68
Katrina Tran <katrinatran21@gmail.com>
2019-10-09 19:35:58 -0700

    Made json file readable in command line

--- spotifind/client.py
+++ spotifind/client.py
@@ -1,6 +1,7 @@
+import sys
 import spotipy
-
-sp = spotipy.Spotify()
+import spotipy.util as util
+import pprint
 
 # import spotipy
 # import spotipy.oauth2 as oauth2
@@ -12,8 +13,6 @@ sp = spotipy.Spotify()
 # token = credentials.get_access_token()
 # spotify = spotipy.Spotify(auth=token)
 
-import spotipy.util as util
-
 scope = 'user-top-read'
 your_username = 'thepunisherspunishing'
 
@@ -24,26 +23,22 @@ token = util.prompt_for_user_token(
         client_secret='bf63ae87996f4f1aad31335abb361d4e',
         redirect_uri='https://www.getpostman.com/oauth2/callback')
 
-spotify = spotipy.Spotify(auth=token)
+sp = spotipy.Spotify(auth=token)
 
 # results = sp.search(q='weezer', limit=20)
 # for i, t in enumerate(results['tracks']['items']):
 #     print(' ', i, t['name'])
 
 top_artists = sp.current_user_top_artists(limit=50, offset=0, time_range='long_term')
+pprint.pprint(top_artists)
 
 
-#
-# current_user_top_artists(limit=20, offset=0, time_range='medium_term')
-# Get the current user’s top artists
-#
 # Parameters:
 # limit - the number of entities to return
 # offset - the index of the first entity to return
 # time_range - Over what time frame are the affinities computed Valid-values: short_term, medium_term, long_term
 # current_user_top_tracks(limit=20, offset=0, time_range='medium_term')
-# Get the current user’s top tracks
-#
+
 # Parameters:
 # limit - the number of entities to return
 # offset - the index of the first entity to return
