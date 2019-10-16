a864014505f577f083a5ba44051e8cf833aa638e
unknown <ra.stanik@hotmail.com>
2019-10-16 11:23:16 -0700

    calling spotify client code from data_handler class and getting audio features from top tracks

--- spotifind/data_handler.py
+++ spotifind/data_handler.py
@@ -1,16 +1,20 @@
-from .client import *
+import json
+from spotifind.client import get_token, get_audio_features, get_top_artists, get_top_tracks
 
 your_username = input("Enter your username: ")
+scope = "user-top-read"
 token = get_token(your_username, scope)
 
 # time_ranges: 'short_term' | 'medium_term' | 'long_term'
-time_range = 'long_term'
-tracks = get_top_tracks(token, time_range)
-print(type(tracks))
+time_range = 'short_term'
+tracks = get_top_tracks(token, 50, time_range)
+items = tracks['items']
+track_ids = []
+for x in items:
+    track_ids.append(x['id'])
 
-artists = get_top_artists(token, time_range)
-print(type(artists))
+print(track_ids)
 
-# features = get_audio_features(token, tracks)
-# print(type(features))
+artists = get_top_artists(token, 50, time_range)
 
+features = get_audio_features(token, track_ids)
