a5d67861293b020cec0e94a36ac49d768e6a3ae8
Katrina Tran <katrinatran21@gmail.com>
2019-11-24 16:46:39 -0800

    added test for get data with no token

--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -1,13 +1,18 @@
 import pytest
 import src.data_handler as dh
 
-# before running tests fill these categories:
-token = 'BQD1SwrIc1vYIxVXQmOGhPbC1LpSsEUtY2k7kOYmOUvuS9ML8iKv-8_YznxX4h8nD4SmMUETaq9V7jfcmdEiOY8u50h8xt2Tw1Azd8svkJPwp81xAYKRRM08r8PKvuInhVeObHF4-HmDdNzC39EfCUphgOejZcpn1g0sCEOXL-w'
-# get token by uncommenting the part where you could get a token, print it out, and then copy here
-username = ''
+class TestDataHandlerNull:
+    def test_no_token_get_data(self):
+        with pytest.raises(Exception):
+            dh.get_data({}, "short_term")
+        assert len(dh.get_top_track_names()) == 0
+        with pytest.raises(Exception):
+            dh.get_data({}, "medium_term")
+        assert len(dh.get_top_artist_names()) == 0
+        with pytest.raises(Exception):
+            dh.get_data({}, "long_term")
+        assert len(dh.get_top_track_names()) == 0
 
-
-class TestClientNull:
     def test_no_token_get_top_genres_data_returns_empty_list(self):
         with pytest.raises(Exception):
             dh.get_user_top_artists({}, "short_term")
@@ -18,6 +23,3 @@ class TestClientNull:
         with pytest.raises(Exception):
             dh.get_user_top_artists({}, "long_term")
         assert len(dh.get_top_genres_data()['top_50_genres_list']) == 0
-
-    def test_with_token_get_top_genres_data_returns_data(self):
-        assert True
